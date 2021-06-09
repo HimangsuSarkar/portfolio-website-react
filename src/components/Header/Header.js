ff6731eedddb74a5b91a48c0bee026872cde39ea
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-06-09 14:01:38 +0800

    header component done successfully

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div>
-           header components 
-        </div>
+        <>
+           <header className="flex justify-between py-1 px-5 absolute w-full">
+               <div className="logo">
+                   <h2 className="text-3xl">
+                   <Link to="/">Himangsu</Link>
+                   </h2>
+               </div>
+               <nav>
+                   <ul className="flex">
+                       <li className="mr-5">
+                           <Link to="/">Home</Link>
+                       </li>
+                       <li className="mr-5">
+                           <Link to="/projects">Projects</Link>
+                       </li>
+                       <li>
+                           <Link to="/hire">Hire</Link>
+                       </li>
+                   </ul>
+               </nav>
+            </header> 
+        </>
     )
 }
 
