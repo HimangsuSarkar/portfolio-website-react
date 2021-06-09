62c092c5c8749430e064f58cab0ca041334e323c
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-06-09 14:36:34 +0800

    showcase component done successfully and some change on header component and index file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 const Header = () => {
     return (
         <>
-           <header className="flex justify-between py-1 px-5 absolute w-full">
+           <header className="flex justify-between py-3 px-5 absolute w-full text-white">
                <div className="logo">
-                   <h2 className="text-3xl">
+                   <h2 className="text-3xl lg:text-5xl">
                    <Link to="/">Himangsu</Link>
                    </h2>
                </div>
@@ -19,7 +19,7 @@ const Header = () => {
                            <Link to="/projects">Projects</Link>
                        </li>
                        <li>
-                           <Link to="/hire">Hire</Link>
+                           <Link to="/hire" className="mr-5 bg-green-500 p-1 px-4 rounded-full font-semibold transition-all hover:bg-green-800">Hire</Link>
                        </li>
                    </ul>
                </nav>
